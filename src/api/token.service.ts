445c5e441d22ea0dfcbe89e61e8d7e277ad33c78
elizaveta.kornilo <elizaveta.kornilo@intexsoft.by>
2022-07-14 21:02:54 +0300

    handle an error during token decoding

--- src/api/token.service.ts
+++ src/api/token.service.ts
@@ -30,12 +30,15 @@ export const getTokenPayload = (): ITokenPayload | null => {
   return decodeToken(token)
 }
 
-const decodeToken = (token: string): ITokenPayload => {
-  const base64Url = token!.split('.')[1]
-  const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
-  const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
-    return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
-  }).join(''))
-  console.log(jsonPayload)
-  return JSON.parse(jsonPayload)
+const decodeToken = (token: string): ITokenPayload | null => {
+  try {
+    const base64Url = token!.split('.')[1]
+    const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+    const jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function (c) {
+      return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+    }).join(''))
+    return JSON.parse(jsonPayload)
+  } catch (e) {
+    return null
+  }
 }
